f5eeef97e0f59dc22559295cf59c822b6d46b35a
Alf Drollinger <alf@drollinger.info>
2023-09-03 23:27:51 +0200

    Filament Resource

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Livewire\Component;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Card;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Actions\EditAction;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Forms\Components\TextInput;
+use App\Filament\Filters\DateRangeFilter;
+use Filament\Tables\Actions\DeleteBulkAction;
+use App\Filament\Resources\UserResource\Pages;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            Card::make()->schema([
+                Grid::make(['default' => 0])->schema([
+                    TextInput::make('name')
+                        ->rules(['max:255', 'string'])
+                        ->required()
+                        ->placeholder('Name')
+                        ->columnSpan([
+                            'default' => 12,
+                            'md' => 12,
+                            'lg' => 12,
+                        ]),
+
+                    TextInput::make('email')
+                        ->rules(['email'])
+                        ->required()
+                        ->unique(
+                            'users',
+                            'email',
+                            fn(?Model $record) => $record
+                        )
+                        ->email()
+                        ->placeholder('Email')
+                        ->columnSpan([
+                            'default' => 12,
+                            'md' => 12,
+                            'lg' => 12,
+                        ]),
+
+                    TextInput::make('password')
+                        ->required()
+                        ->password()
+                        ->dehydrateStateUsing(fn($state) => \Hash::make($state))
+                        ->required(
+                            fn(Component $livewire) => $livewire instanceof
+                                Pages\CreateUser
+                        )
+                        ->placeholder('Password')
+                        ->columnSpan([
+                            'default' => 12,
+                            'md' => 12,
+                            'lg' => 12,
+                        ]),
+                ]),
+            ]),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->poll('60s')
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->toggleable()
+                    ->searchable(true, null, true)
+                    ->limit(50),
+                Tables\Columns\TextColumn::make('email')
+                    ->toggleable()
+                    ->searchable(true, null, true)
+                    ->limit(50),
+            ])
+            ->filters([DateRangeFilter::make('created_at')])
+            ->actions([ViewAction::make(), EditAction::make()])
+            ->bulkActions([DeleteBulkAction::make()]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'view' => Pages\ViewUser::route('/{record}'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
